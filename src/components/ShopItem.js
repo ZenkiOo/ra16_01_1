eea50834bbca447248f20dda6b02b7fad4948114
ZenkiOo <zenky@list.ru>
2022-09-06 16:56:35 +0300

    fix

---
+++ src/components/ShopItem.js
@@ -0,0 +1,29 @@
+import PropTypes from 'prop-types';
+
+function ShopItem(props) {
+  const { item } = props;
+  return (
+    <div className="main-content">
+      <h2>{item.brand}</h2>
+      <h1>{item.title}</h1>
+      <h3>{item.description}</h3>
+      <div className="description">{item.descriptionFull}</div>
+      <div className="highlight-window mobile">
+        <div className="highlight-overlay"></div>
+      </div>
+      <div className="divider"></div>
+      <div className="purchase-info">
+        <div className="price">{`${item.currency}${item.price.toFixed(
+          2
+        )}`}</div>
+        <button>Добавить в корзину</button>
+      </div>
+    </div>
+  );
+}
+
+ShopItem.propTypes = {
+  item: PropTypes.object,
+};
+
+export default ShopItem;
